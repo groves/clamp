c187c0d1e4b6e61dd58bca4fc9cd12c190529070
Charlie Groves <charlie.groves@gmail.com>
2009-01-12 22:15:30 -0800

    Simplify

--- src/python/clamp/__init__.py
+++ src/python/clamp/__init__.py
@@ -15,17 +15,16 @@ class Clamper(type):
                     builder = InterfaceBuilder("I" + name)
                 javaReturn, javaArgs = v._clamp
                 builder.addMethod(k, javaReturn, javaArgs, [])
-        if builder is None:
+        if builder is None:# No new clamped methods on a subinterface, let it be
             return type.__new__(meta, name, bases, dict)
-        if builder is not None:
-            iface = builder.load()
-            newbases = []
-            for base in bases:
-                if type(base) == meta:
-                    newbases.append(iface)
-                else:
-                    newbases.append(base)
-            bases = tuple(newbases)
+        iface = builder.load()
+        newbases = []
+        for base in bases:
+            if type(base) == meta:# Remove the meta-type; PyType can't handle it yet
+                newbases.append(iface)
+            else:
+                newbases.append(base)
+        bases = tuple(newbases)
         return type.__new__(type, name, bases, dict)
 
 class Clamp(object):
