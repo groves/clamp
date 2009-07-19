54b634ca861b5e05313fd58e2f5649148638b099
Charlie Groves <charlie.groves@gmail.com>
2009-07-19 12:14:51 -0700

    Subclasses of subclasses

--- src/python/clamp/test/test_subclassing.py
+++ src/python/clamp/test/test_subclassing.py
@@ -1,6 +1,11 @@
+from java.lang import Integer, UnsupportedOperationException
 from java.util import AbstractList
-from clamp import Clamp
-from org.sevorg.clamp import TestSubclassing
+
+from nose.tools import assert_raises, eq_
+
+from clamp import Clamp, java
+from org.sevorg.clamp import Reflector
+
 
 class ClampedList(Clamp, AbstractList):
     def get(self, idx):
@@ -10,4 +15,17 @@ class ClampedList(Clamp, AbstractList):
         return 10
 
 def test_extendingclass():
-    TestSubclassing.manipulate(ClampedList())
+    clamped = Reflector.instantiate(ClampedList, [], [])
+    eq_(5, Reflector.call(clamped, 'get', [Integer.TYPE], [5]))
+    assert_raises(UnsupportedOperationException, clamped.add, 5)
+
+# TODO - Make Clamp the metatype on ClampedList so we don't need to use it again here
+class ExtraMethodList(Clamp, ClampedList):
+    @java(Integer.TYPE)
+    def last(self):
+        return self[-1]
+
+def test_extendingclamped():
+    extra = Reflector.instantiate(ExtraMethodList, [], [])
+    last = Reflector.call(extra, "last", [], [])
+    eq_(9, last)
